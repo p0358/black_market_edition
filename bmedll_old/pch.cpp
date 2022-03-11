84455a026c5212cb727297642b2e26d43d45e8be
p0358 <p0358@users.noreply.github.com>
2022-03-11 16:56:38 +0100

    remove bmedll_old, x86 configs; partial update to vs2022

--- bmedll_old/pch.cpp
+++
@@ -1,5 +0,0 @@
-﻿// pch.cpp: plik źródłowy odpowiadający wstępnie skompilowanemu nagłówkowi
-
-#include "pch.h"
-
-// W przypadku korzystania ze wstępnie skompilowanych nagłówków ten plik źródłowy jest niezbędny, aby kompilacja powiodła się.
