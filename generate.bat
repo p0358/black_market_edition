fc27d5fa55974df8c8fd0862c49ea42e6749ed73
p0358 <p0358@users.noreply.github.com>
2025-12-29 06:48:18 +0100

    update deps

--- generate.bat
+++ generate.bat
@@ -3,5 +3,5 @@
 :: This generates Visual Studio projects for building the C++ parts.
 
 git submodule update --init
-powershell -ExecutionPolicy Bypass -Command "scripts\get-premake.ps1 5.0.0-beta4"
+powershell -ExecutionPolicy Bypass -Command "scripts\get-premake.ps1 5.0.0-beta7"
 premake\premake5 vs2022 %*
